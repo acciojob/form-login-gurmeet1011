782a1711e171cfa288e9ab62763cc9f7415d53b1
gurmeet1011 <156289823+gurmeet1011@users.noreply.github.com>
2024-10-20 12:05:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementById('form1');
 
+    // Extract the values of first name and last name using the name attribute
+    const firstName = form.elements['fname'].value;
+    const lastName = form.elements['lname'].value;
+
+    // Display the full name in an alert
+    alert(`${firstName} ${lastName}`);
 }
